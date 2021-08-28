0a98e3359dae95096fea120a22d557c8f599f019
Gaurav <gauravmalviya89@yahoo.in>
2021-08-28 15:45:16 +0530

    configure github actions

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require ('express') // Importing Express
 const app = express(); // Initialize Express
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
 
 app.get('/names', (req, res) => {
     res.send("Gaurav");
